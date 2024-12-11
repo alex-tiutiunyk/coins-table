b25f1ddefe86d6eab4809bf6475725918d701ce5
Alex Tiutiunyk <alex.tiutiunyk@gmail.com>
2024-12-11 11:43:40 +0200

    Add routing

---
+++ src/pages/Login.jsx
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom"
+
+const Login = () => {
+  return (
+    <>
+      Login
+      <Link to="/coins-table/bot">Bot</Link>
+    </>
+  )
+}
+
+export default Login
