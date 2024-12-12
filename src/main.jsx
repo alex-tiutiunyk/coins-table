1a98bb2cb6726a68ad4e5506f90e687dd46ad6f6
Alex Tiutiunyk <alex.tiutiunyk@gmail.com>
2024-12-12 11:29:11 +0200

    Merge changes from main

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,17 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
+// import { BrowserRouter} from 'react-router-dom'
+import { Provider } from 'react-redux'
+import { store } from './store'
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
+  <Provider store={store}>
+    {/* <BrowserRouter> */}
+    <HashRouter>
       <App />
-  </BrowserRouter>
+    </HashRouter>
+    {/* </BrowserRouter> */}
+  </Provider>
 )
